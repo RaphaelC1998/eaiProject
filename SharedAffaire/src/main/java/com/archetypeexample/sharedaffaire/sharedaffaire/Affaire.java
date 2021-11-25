5cb0e2cfb367b1f2af6edf71b785ade32961cd66
Chambraud Raphaël <raphael.c360@gmail.com>
2021-11-25 15:52:43 +0100

    second commit

---
+++ SharedAffaire/src/main/java/com/archetypeexample/sharedaffaire/sharedaffaire/Affaire.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.archetypeexample.sharedaffaire.sharedaffaire;
+import java.io.Serializable;
+import java.util.ArrayList;
+/**
+ *
+ * @author Raph
+ */
+public class Affaire implements Serializable {
+    
+    private int numAffaire;
+    
+}
